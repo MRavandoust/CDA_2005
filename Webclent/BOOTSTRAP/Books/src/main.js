e573734ffdb151bfed0366fad5d0126ab1f97d28
MRavandoust <akam_50@yahoo.com>
2020-09-27 23:35:12 +0200

    Books project - Bootstrap

---
+++ Webclent/BOOTSTRAP/Books/src/main.js
@@ -0,0 +1,49 @@
+/**Sticky Navigation */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+    let oTop = $(".section-2").offset().top - window.innerHeight;
+    if ($(window).scrollTop() > oTop) {
+        navbar.addClass("sticky");
+    } else {
+        navbar.removeClass("sticky");
+    }
+});
+
+
+
+
+
+
+
+/**Counter Animation */
+
+function mCount(selector) {
+
+    $(selector).each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+};
+
+
+
+
+//let a = 0;
+$(window).scroll(function () {
+    let oTop = $(".numbers").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+        a++;
+        //nCount(".rect > h1");
+
+    }
+})
