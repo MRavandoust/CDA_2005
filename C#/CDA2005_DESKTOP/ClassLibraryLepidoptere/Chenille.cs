48d3b500b3e7b7233cc4a43bb7669e7dd292e243
MRavandoust <akam_50@yahoo.com>
2020-10-07 15:39:37 +0200

    Lepidoptere

---
+++ C#/CDA2005_DESKTOP/ClassLibraryLepidoptere/Chenille.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryLepidoptere
+{
+    public class Chenille : StadeDevolution
+    {
+        public override bool SeDeplacer()
+        {
+            Console.WriteLine("Je rampe");
+            return true;
+        }
+    }
+}
---
+++ C#/CDA2005_DESKTOP/ClassLibraryLepidoptere/StadeDevolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryLepidoptere
+{
+    abstract public class StadeDevolution
+    {
+        abstract public bool SeDeplacer();
+
+        public bool PrendreDuPoids()
+        {
+            return true;
+        }
+       
+    }
+}
