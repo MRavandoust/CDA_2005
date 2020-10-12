6de85f64b9da4d05ee6f881ccc7282d78e0ad713
MRavandoust <akam_50@yahoo.com>
2020-10-12 13:15:24 +0200

    Exo Feuilles

---
+++ C#/CDA2005_DESKTOP/ClassLibraryForet/Arbre.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForet
+{
+    public class Arbre
+    {
+        int hauteur;
+        private List<Feuille> feuilles = new List<Feuille>();
+
+        public Arbre(int hauteur)
+        {
+            this.hauteur = hauteur;
+            //this.feuilles = new List<Feuille>();
+        }
+
+        public Arbre(int hauteur, List<Feuille> feuilles) //ouvre porte aggregation
+        {
+            //a faire
+            this.hauteur = hauteur;
+            this.feuilles = feuilles;
+        }
+
+        public void AddFeuille(Feuille f)
+        {
+            //a faire
+            feuilles.Add(f);
+        }
+
+        public int Hauteur { get => hauteur; 
+            //set => hauteur = value; 
+        }
+    }
+}
---
+++ C#/CDA2005_DESKTOP/ClassLibraryForet/Feuille.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForet
+{
+    public class Feuille
+    {
+        int nbNervures;
+        private EnumFormeFeuille formeDeFeuille;
+        private EnumCouleur couleurDeFeuille;
+
+        public Feuille(int nbNervures)
+        {
+            this.nbNervures = nbNervures;
+        }
+
+        public Feuille(int nbNervures, EnumFormeFeuille formeDeFeuille, EnumCouleur couleurDeFeuille)
+        {
+            this.nbNervures = nbNervures;
+            this.formeDeFeuille = EnumFormeFeuille.cordineForme;
+            this.couleurDeFeuille = EnumCouleur.Vert;
+        }
+
+        public int NbNervures { get => nbNervures; 
+            //set => nbNervures = value; 
+        }
+
+        public void Tomber()
+        {
+            Console.WriteLine("Je tombe");
+        }
+
+        public override string ToString()
+        {
+            return "Nombre de nervures: " + nbNervures + " Forme de feuille: " + formeDeFeuille + " Couleur de feuille: " + couleurDeFeuille;
+        }
+    }
+}
