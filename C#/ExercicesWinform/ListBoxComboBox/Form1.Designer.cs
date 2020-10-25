ab6baf081752fac8a65093f087b463b745630713
MRavandoust <akam_50@yahoo.com>
2020-10-25 16:10:41 +0100

    ListBox et ComboBox

---
+++ C#/ExercicesWinform/ListBoxComboBox/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace ListBoxComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSource = new System.Windows.Forms.Label();
+            this.lblCible = new System.Windows.Forms.Label();
+            this.comboSource = new System.Windows.Forms.ComboBox();
+            this.listBoxCible = new System.Windows.Forms.ListBox();
+            this.btnDroite = new System.Windows.Forms.Button();
+            this.btnDroiteAll = new System.Windows.Forms.Button();
+            this.btnGaucheAll = new System.Windows.Forms.Button();
+            this.btnGauche = new System.Windows.Forms.Button();
+            this.btnBas = new System.Windows.Forms.Button();
+            this.btnHaut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(97, 18);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(51, 16);
+            this.lblSource.TabIndex = 0;
+            this.lblSource.Text = "Source";
+            // 
+            // lblCible
+            // 
+            this.lblCible.AutoSize = true;
+            this.lblCible.Location = new System.Drawing.Point(445, 18);
+            this.lblCible.Name = "lblCible";
+            this.lblCible.Size = new System.Drawing.Size(39, 16);
+            this.lblCible.TabIndex = 0;
+            this.lblCible.Text = "Cible";
+            // 
+            // comboSource
+            // 
+            this.comboSource.FormattingEnabled = true;
+            this.comboSource.Items.AddRange(new object[] {
+            "France",
+            "Belgique",
+            "Allemagne",
+            "Japon",
+            "Portugal",
+            "Grece",
+            "Espagne"});
+            this.comboSource.Location = new System.Drawing.Point(11, 47);
+            this.comboSource.Name = "comboSource";
+            this.comboSource.Size = new System.Drawing.Size(227, 24);
+            this.comboSource.TabIndex = 0;
+            this.comboSource.SelectedIndexChanged += new System.EventHandler(this.comboSource_SelectedIndexChanged);
+            // 
+            // listBoxCible
+            // 
+            this.listBoxCible.FormattingEnabled = true;
+            this.listBoxCible.ItemHeight = 16;
+            this.listBoxCible.Location = new System.Drawing.Point(347, 47);
+            this.listBoxCible.Name = "listBoxCible";
+            this.listBoxCible.Size = new System.Drawing.Size(210, 244);
+            this.listBoxCible.TabIndex = 5;
+            this.listBoxCible.SelectedIndexChanged += new System.EventHandler(this.listBoxCible_SelectedIndexChanged);
+            // 
+            // btnDroite
+            // 
+            this.btnDroite.Location = new System.Drawing.Point(258, 47);
+            this.btnDroite.Name = "btnDroite";
+            this.btnDroite.Size = new System.Drawing.Size(69, 24);
+            this.btnDroite.TabIndex = 1;
+            this.btnDroite.Text = ">";
+            this.btnDroite.UseVisualStyleBackColor = true;
+            this.btnDroite.Click += new System.EventHandler(this.btnDroite_Click);
+            // 
+            // btnDroiteAll
+            // 
+            this.btnDroiteAll.Location = new System.Drawing.Point(258, 77);
+            this.btnDroiteAll.Name = "btnDroiteAll";
+            this.btnDroiteAll.Size = new System.Drawing.Size(69, 24);
+            this.btnDroiteAll.TabIndex = 4;
+            this.btnDroiteAll.Text = ">>";
+            this.btnDroiteAll.UseVisualStyleBackColor = true;
+            this.btnDroiteAll.Click += new System.EventHandler(this.btnDroiteAll_Click);
+            // 
+            // btnGaucheAll
+            // 
+            this.btnGaucheAll.Location = new System.Drawing.Point(258, 267);
+            this.btnGaucheAll.Name = "btnGaucheAll";
+            this.btnGaucheAll.Size = new System.Drawing.Size(69, 24);
+            this.btnGaucheAll.TabIndex = 3;
+            this.btnGaucheAll.Text = "<<";
+            this.btnGaucheAll.UseVisualStyleBackColor = true;
+            this.btnGaucheAll.Click += new System.EventHandler(this.btnGaucheAll_Click);
+            // 
+            // btnGauche
+            // 
+            this.btnGauche.Location = new System.Drawing.Point(258, 237);
+            this.btnGauche.Name = "btnGauche";
+            this.btnGauche.Size = new System.Drawing.Size(69, 24);
+            this.btnGauche.TabIndex = 5;
+            this.btnGauche.Text = "<";
+            this.btnGauche.UseVisualStyleBackColor = true;
+            this.btnGauche.Click += new System.EventHandler(this.btnGauche_Click);
+            // 
+            // btnBas
+            // 
+            this.btnBas.Image = global::ListBoxComboBox.Properties.Resources.Down;
+            this.btnBas.Location = new System.Drawing.Point(461, 297);
+            this.btnBas.Name = "btnBas";
+            this.btnBas.Size = new System.Drawing.Size(31, 41);
+            this.btnBas.TabIndex = 7;
+            this.btnBas.UseVisualStyleBackColor = true;
+            this.btnBas.Click += new System.EventHandler(this.btnBas_Click);
+            // 
+            // btnHaut
+            // 
+            this.btnHaut.Image = global::ListBoxComboBox.Properties.Resources.Up1;
+            this.btnHaut.Location = new System.Drawing.Point(416, 297);
+            this.btnHaut.Name = "btnHaut";
+            this.btnHaut.Size = new System.Drawing.Size(31, 41);
+            this.btnHaut.TabIndex = 6;
+            this.btnHaut.UseVisualStyleBackColor = true;
+            this.btnHaut.Click += new System.EventHandler(this.btnHaut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 359);
+            this.Controls.Add(this.btnBas);
+            this.Controls.Add(this.btnHaut);
+            this.Controls.Add(this.btnDroiteAll);
+            this.Controls.Add(this.btnGauche);
+            this.Controls.Add(this.btnGaucheAll);
+            this.Controls.Add(this.btnDroite);
+            this.Controls.Add(this.listBoxCible);
+            this.Controls.Add(this.comboSource);
+            this.Controls.Add(this.lblCible);
+            this.Controls.Add(this.lblSource);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Label lblCible;
+        private System.Windows.Forms.ComboBox comboSource;
+        private System.Windows.Forms.ListBox listBoxCible;
+        private System.Windows.Forms.Button btnDroite;
+        private System.Windows.Forms.Button btnDroiteAll;
+        private System.Windows.Forms.Button btnGaucheAll;
+        private System.Windows.Forms.Button btnGauche;
+        private System.Windows.Forms.Button btnHaut;
+        private System.Windows.Forms.Button btnBas;
+    }
+}
+
---
+++ C#/ExercicesWinform/ListBoxComboBox/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnDroite.Enabled = false;
+            btnHaut.Enabled = false;
+            btnGauche.Enabled = false;
+            btnBas.Enabled = false;
+        }
+
+        private void comboSource_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnDroite.Enabled = true;
+        }
+
+        private void btnDroite_Click(object sender, EventArgs e)
+        {
+            listBoxCible.Items.Add(comboSource.SelectedItem.ToString());
+            comboSource.Items.RemoveAt(comboSource.SelectedIndex);
+            btnDroite.Enabled = false;
+        }
+
+        private void btnDroiteAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < comboSource.Items.Count; i++)
+            {
+                listBoxCible.Items.Add(comboSource.Items[i]);
+            }
+            comboSource.Items.Clear();
+            btnDroiteAll.Enabled = false;
+            btnGaucheAll.Enabled = true;
+        }
+
+        private void listBoxCible_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnGauche.Enabled = true;
+            if (listBoxCible.SelectedIndex > 0)
+                btnHaut.Enabled = true;
+            else
+                btnHaut.Enabled = false;
+            if (listBoxCible.SelectedIndex < listBoxCible.Items.Count - 1)
+                btnBas.Enabled = true;
+            else
+                btnBas.Enabled = false;
+        }
+
+        private void btnGauche_Click(object sender, EventArgs e)
+        {
+            comboSource.Items.Add(listBoxCible.SelectedItem.ToString());
+            listBoxCible.Items.RemoveAt(listBoxCible.SelectedIndex);
+            btnGauche.Enabled = false;
+        }
+
+        private void btnGaucheAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBoxCible.Items.Count; i++)
+            {
+                comboSource.Items.Add(listBoxCible.Items[i]);
+            }
+            listBoxCible.Items.Clear();
+            btnGaucheAll.Enabled = false;
+            btnDroiteAll.Enabled = true;
+        }
+
+        private void btnHaut_Click(object sender, EventArgs e)
+        {
+            int index = listBoxCible.SelectedIndex;
+            string temp = listBoxCible.Items[index - 1].ToString();
+            listBoxCible.Items[index - 1] = listBoxCible.Items[index];
+            listBoxCible.Items[index] = temp;
+            listBoxCible.SelectedItem = listBoxCible.Items[index - 1];
+
+        }
+
+        private void btnBas_Click(object sender, EventArgs e)
+        {
+            int index = listBoxCible.SelectedIndex;
+            string temp = listBoxCible.Items[index + 1].ToString();
+            listBoxCible.Items[index + 1] = listBoxCible.Items[index];
+            listBoxCible.Items[index] = temp;
+            listBoxCible.SelectedItem = listBoxCible.Items[index + 1];
+        }
+    }
+}
