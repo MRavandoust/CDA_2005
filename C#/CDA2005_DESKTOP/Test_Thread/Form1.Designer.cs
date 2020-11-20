a4851bdf78cf2c71f8ead4d00af7d7640cda3c7e
MRavandoust <akam_50@yahoo.com>
2020-11-20 16:12:45 +0100

    Thread testing

---
+++ C#/CDA2005_DESKTOP/Test_Thread/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Test_Thread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_synchrone = new System.Windows.Forms.Button();
+            this.btn_Asynchrone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_synchrone
+            // 
+            this.btn_synchrone.Location = new System.Drawing.Point(55, 54);
+            this.btn_synchrone.Name = "btn_synchrone";
+            this.btn_synchrone.Size = new System.Drawing.Size(127, 34);
+            this.btn_synchrone.TabIndex = 0;
+            this.btn_synchrone.Text = "Synchrone";
+            this.btn_synchrone.UseVisualStyleBackColor = true;
+            this.btn_synchrone.Click += new System.EventHandler(this.btn_synchrone_Click);
+            // 
+            // btn_Asynchrone
+            // 
+            this.btn_Asynchrone.Location = new System.Drawing.Point(244, 54);
+            this.btn_Asynchrone.Name = "btn_Asynchrone";
+            this.btn_Asynchrone.Size = new System.Drawing.Size(127, 34);
+            this.btn_Asynchrone.TabIndex = 0;
+            this.btn_Asynchrone.Text = "Asynchrone";
+            this.btn_Asynchrone.UseVisualStyleBackColor = true;
+            this.btn_Asynchrone.Click += new System.EventHandler(this.btn_Asynchrone_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 156);
+            this.Controls.Add(this.btn_Asynchrone);
+            this.Controls.Add(this.btn_synchrone);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_synchrone;
+        private System.Windows.Forms.Button btn_Asynchrone;
+    }
+}
+
---
+++ C#/CDA2005_DESKTOP/Test_Thread/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Test_Thread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        static void my_thread1()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 100000000; i++)
+            {
+                int number = rnd.Next(10000, 20000);
+                i += number;
+                i -= number;
+            }
+            MessageBox.Show("Thread 1: " + DateTime.Now.ToUniversalTime());
+        }
+
+        static void my_thread2()
+        {
+            for (int i = 0; i < 100000000; i++)
+            {
+
+            }
+            MessageBox.Show("Thread 2: " + DateTime.Now.ToUniversalTime());
+        }
+
+        private void btn_Asynchrone_Click(object sender, EventArgs e)
+        {
+            btn_Asynchrone.Enabled = false;
+            btn_synchrone.Enabled = false;
+
+            Thread t1 = new Thread(my_thread1);
+            Thread t2 = new Thread(my_thread2);
+            t1.Start();
+            t2.Start();
+
+            btn_Asynchrone.Enabled = true;
+            btn_synchrone.Enabled = true;
+        }
+
+        private void btn_synchrone_Click(object sender, EventArgs e)
+        {
+            btn_Asynchrone.Enabled = false;
+            btn_synchrone.Enabled = false;
+
+            my_thread1();
+            my_thread2();
+
+            btn_Asynchrone.Enabled = true;
+            btn_synchrone.Enabled = true;
+        }
+    }
+}
